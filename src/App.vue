590f4783f632c0f18709c6e99038ba2840d9a738
k24abhi-griet <20241a05x0@griet.ac.in>
2025-11-10 01:25:41 +0530

    adding files

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <FinancialReportTreeTable />
+</template>
+
+<script>
+import FinancialReportTreeTable from './components/FinancialReportTreeTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FinancialReportTreeTable
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Inter', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #f5f7fa;
+  min-height: 100vh;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
